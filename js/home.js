0f2b33a26508e64c377eeeb7facd42ead5aeca40
abusayadebu <abusayad743@gmail.com>
2024-09-27 18:30:44 +0600

    input geting done for add money form

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log('home added')
+
+// add money to the account
+
+// step-1: add money button working or not
+document.getElementById('btn-add-money')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    
+    // step:2-- get money from input
+    const inputMoney = document.getElementById('input-amount').value;
+    const inputPin = document.getElementById('input-pin').value;
+
+    console.log(inputMoney, inputPin)
+    
+})
