a4790c946780fc8555823b92f48c4684023c0b33
abusayadebu <abusayad743@gmail.com>
2024-09-27 17:32:51 +0600

    validation check and home navigation done

--- js/login.js
+++ js/login.js
@@ -1,10 +1,20 @@
 document.getElementById('login-button')
 .addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login button clicked')
 
-    // get the phone number
+    // get the phone number and pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber)
+    // console.log(pinNumber)
+
+    // bad way to validate the credentials (only for learning purpose)
+    if(phoneNumber === '0123456789' && pinNumber === '1234'){
+        console.log('You are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong Credencials');
+    }
     
 })
